6e2572a01771e5a357e0aab1240cac2074127444
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2023-04-03 16:23:01 +0900

    Yangi funksiya qo'shildi
    otgan_vaqt funksiyasi

--- python_st_libraries.py
+++ python_st_libraries.py
@@ -5,11 +5,42 @@ Spyder Editor
 This is a temporary script file.
 """
 import datetime as dt
+import math as m
+
+# # Bugungi sanadan boshlab 2 hafta farq bilan 10 ta sanani konsolga chiqaring
+# bugun = dt.date.today()
+# for _ in range(10):
+#     bugun = bugun + dt.timedelta(days=14)
+#     print(bugun)
+    
+    
+# # Ramazon va qurbon hayitigacha qolgan kunlarni konsolga chiqaring
+# bugun = dt.date.today()
+# ramazon_h = dt.date(2023, 4, 22)
+# farq = ramazon_h - bugun
+# print(farq)
+# print(f"Ramazon Hayitiga {farq.days} kun qoldi.")
 
 
-# Bugungi sanadan boshlab 2 hafta farq bilan 10 ta sanani konsolga chiqaring
+# # Tug'ilgan kuningizdan bugungi sanagacha qancha yil, oy, kun o'tganini 
+# # qaytaruvchi funksiya yozing
+# def otgan_vaqt(yyyy, mm, d):
+#     t_kun = dt.date(yyyy, mm, d)
+#     bugun = dt.date.today()
+    
+#     time_passed = bugun - t_kun
+#     yil = dt.timedelta(days=365)
+#     oy = dt.timedelta(days=30)
+    
+#     yillar = m.floor(time_passed / yil)
+#     oylar = m.floor((time_passed % yil)/ oy)
+#     kunlar = (time_passed % oy).days
+    
+#     print(f"Tug'ilgan kuningizdan hozirgacha {yillar} yil, {oylar} oy, {kunlar} kun o'tdi.")
+    
+    
+# otgan_vaqt(1964, 11, 25)
 
-bugun = dt.date.today()
-for _ in range(10):
-    bugun = bugun + dt.timedelta(days=14)
-    print(bugun)
+
+# Foydalanuvchidan telefon raqamini kiritishni so'rang. 
+# Kiritlgan qiymatni andoza yordamida tekshiring
